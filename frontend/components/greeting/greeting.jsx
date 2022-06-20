28d6e6d444b9b62a79c7c74d57577cb9d0f4f848
nilshen <linshen88@gmail.com>
2022-06-20 13:59:55 -0400

    prepare for initial heroku

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -7,6 +7,7 @@ const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <nav className="login-signup">
       <Link to="/login">Login</Link>
+      <br />
       <Link to="/signup">Sign up!</Link>
     </nav>
   );
